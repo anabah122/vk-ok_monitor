e3ff987a02389da9674bc776549c27635eb4735c
anabah122 <roflercast@mail.ru>
2026-04-10 16:54:38 +0300

    paths

--- _G.py
+++ _G.py
@@ -3,8 +3,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 CONFIRMATION_CODE_VK = os.environ["CONFIRMATION_CODE_VK"]
 SECRET_KEY_VK_GROUP  = os.environ["SECRET_KEY_VK_GROUP"]
 
 LAST_ACTION_TIMESTAMP = time.time()
-DB_PATH = "DB/db.db"
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DB_PATH  = os.path.join(BASE_DIR, "DB", "db.db")
