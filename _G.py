118ae07bd0777198653d8438ad5e3c4a8e18a5c2
anabah122 <roflercast@mail.ru>
2026-04-10 20:23:28 +0300

    fix env db in _G

--- _G.py
+++ _G.py
@@ -9,7 +9,5 @@ SECRET_KEY_VK_GROUP  = os.environ["SECRET_KEY_VK_GROUP"]
 
 LAST_ACTION_TIMESTAMP = time.time()
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DB_PATH  = os.path.join(BASE_DIR, "DB", "db.db")
-
-DB_NAME = os.environ.get("DB_NAME", "db.db")
-DB_PATH = os.path.join(BASE_DIR, "DB", DB_NAME)
+DB_NAME  = os.environ["DB_NAME"]
+DB_PATH  = os.path.join(BASE_DIR, "DB", DB_NAME)
