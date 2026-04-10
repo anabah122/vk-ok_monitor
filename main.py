aeee29b24e33571a8a758aaff72397047b231b96
anabah122 <roflercast@mail.ru>
2026-04-11 02:12:55 +0300

    fix docker req

--- main.py
+++ main.py
@@ -36,5 +36,6 @@ if __name__ == "__main__":
     import uvicorn
     uvicorn.run("main:app", host="0.0.0.0", port=5000, reload=True)
 
-#cd /app && git pull origin main && docker compose up -d --build
-#docker logs app-app-1
+# ssh root@85.204.240.73
+# cd /app && git pull origin main && docker compose up -d --build
+# docker logs app-app-1
